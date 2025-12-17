cf5ff773c7ce97284d2ade55e1409f495b2c284e
BRCorg <guvenberancan1@gmail.com>
2025-12-17 13:00:40 +0100

    feat:Préparation de la page Register

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,18 @@
+// import Footer from "../../layouts/Footer";
+// import Header from "../../layouts/Header";
+
+const Register = () => {
+
+  return (
+    <>
+        <div className="">
+            {/* <Header /> */}
+            {/* <Main/> */}
+            {/* <Footer/> */}
+        </div>
+
+    </>
+  );
+};
+
+export default Register;
