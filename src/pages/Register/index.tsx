2984a8f011b2f22ab1daf22784fe4d3e511d2743
BRCorg <guvenberancan1@gmail.com>
2025-12-18 11:16:28 +0100

    feat:Quick Fix

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,20 +1,16 @@
 import Footer from "../../layouts/Footer";
 import Header from "../../layouts/Header";
 import Main from "../../layouts/Main";
- 
+
 const Register = () => {
- 
   return (
     <>
-        <div className="">
-            <Header />
-            <Main/>
-            <Footer/>
-        </div>
- 
+      <Header />
+      <Main />
+      <Footer />
+
     </>
-  );
-};
- 
+  )
+}
+
 export default Register;
- 
