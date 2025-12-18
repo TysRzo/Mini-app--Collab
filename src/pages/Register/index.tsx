145720b0579a0f9da12aafca3d5a8fe8a548cce8
BRCorg <guvenberancan1@gmail.com>
2025-12-18 10:38:26 +0100

    feat:Quick Fix

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,12 +1,15 @@
+import Footer from "../../layouts/Footer";
+import Header from "../../layouts/Header";
+import Main from "../../layouts/Main";
 
 const Register = () => {
 
   return (
     <>
         <div className="">
-            {/* <Header /> */}
-            {/* <Main/> */}
-            {/* <Footer/> */}
+            <Header />
+            <Main/>
+            <Footer/>
         </div>
 
     </>
