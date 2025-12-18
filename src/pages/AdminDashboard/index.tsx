23f11075c7f9ec695c1f9540a11ace44d8cd9ec0
Sokhna NDIONE <sokhna.ndione@3wacademy.fr>
2025-12-18 11:18:08 +0100

    feat: Modifs code page admin

--- src/pages/AdminDashboard/index.tsx
+++ src/pages/AdminDashboard/index.tsx
@@ -2,11 +2,11 @@ import Footer from "../../layouts/Footer";
 import Header from "../../layouts/Header";
 import Main from "../../layouts/Main";
 
-function Dashboard() {
+const Dashboard = () => {
   return (
     <>
       < Header />
-      < Main  />
+      < Main />
       < Footer />
 
     </>
