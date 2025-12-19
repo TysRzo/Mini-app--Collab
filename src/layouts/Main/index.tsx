f2cd9121c3fb642406b3dae7a15c52988f0f24aa
Nicolas vauché <hello@nicolasvauche.net>
2025-12-19 09:07:46 +0100

    Fix: Main width

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -6,7 +6,7 @@ type MainProps = {
 
 const Main = ({ children }: MainProps) => {
   return (
-    <main className="min-h-screen bg-slate-50 px-4 py-8 sm:px-6 lg:px-8">
+    <main className="mx-auto h-16 max-w-7xl min-h-screen bg-slate-50 px-4 py-8 sm:px-6 lg:px-8">
       {children}
     </main>
   );
